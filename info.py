1ddad418c8d8f7ea141834d3848d311116a8ac9e
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-03-29 16:21:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+cSQttBg3dh02ZGFl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+DkJ1W6LHiaxhZTg1')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+bZFqbCxTU0kzNTU1') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Morning006)
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Morning006')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
